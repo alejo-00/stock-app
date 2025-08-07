438062a787caba54adec2643c2b87c813260d718
alejo-00 <alejolescano3@gmail.com>
2025-08-06 22:28:53 -0300

    v1.0: incorporacion del nav menu

--- public/scripts.js
+++ public/scripts.js
@@ -1,7 +1,24 @@
 //ELEMENTOS DEL DOM
+const btnMenu = document.getElementById("btn-menu");
+const listMenu = document.getElementById("list-menu");
+const btnAgregar = document.getElementById("btn-agregar-producto");
+const btnModificar = document.getElementById("btn-modificar-producto");
+const btnEliminar = document.getElementById("btn-eliminar-producto");
 const tabla = document.getElementById("miTabla");
 
+//FUNCIONES
+const mostrarMenu = () => {
+  if (listMenu.hasAttribute("hidden")) {
+    listMenu.removeAttribute("hidden");
+  }
+  else {
+    listMenu.setAttribute("hidden", "");
+  };
+};
+
+//READY
 $(document).ready(function() {
-    console.log("Document is ready!");
+    console.log("Ready!");
     $(tabla).DataTable();
+    btnMenu.addEventListener("click", mostrarMenu);
   });
